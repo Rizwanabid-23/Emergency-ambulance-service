c3afe8437f6498a32124f97db904d00142f6f6cd
Rizwanabid-23 <78040827+Rizwanabid-23@users.noreply.github.com>
2021-12-08 22:41:39 +0500

    frontend-UI design-Login form, Admin form

---
+++ Emergency Ammbulance Service/admin_main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emergency_Ammbulance_Service
+{
+    public partial class admin_main : Form
+    {
+        public admin_main()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pin_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_user_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void log_out(object sender, EventArgs e)
+        {
+            this.Hide();
+            loginForm f = new loginForm();
+            f.Show();
+        }
+    }
+}
---
+++ Emergency Ammbulance Service/login_form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emergency_Ammbulance_Service
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_clicked(object sender, EventArgs e)
+        {
+            string username = login_user_box.Text;
+            string password = login_password.Text;
+            bool check = verify_user(username, password);
+            if(check)
+            {
+                this.Hide();
+                admin_main admin = new admin_main();
+                admin.Show();
+            }
+            else
+            {
+                this.Hide();
+                failed f = new failed();
+                f.Show();
+            }
+
+        }
+        private bool verify_user(string username, string password)
+        {
+            if(username=="admin" && password=="123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
