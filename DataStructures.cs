92293cad911c0f30989df90da31616ae2f0d50c8
Umair-Manzoor-47 <2020cs24@student.uet.edu.pk>
2021-12-08 23:22:56 +0500

    Backend-Added-DataStructure(Queue)

--- DataStructures.cs
+++ DataStructures.cs
@@ -69,7 +69,64 @@ namespace dsa2021g34
 
 
  
+class Queue
+    {
+        public Node head;
+        public Node tail;
+        public int QueueSize;
 
+        public Queue() {
+            this.head = null;
+            this.tail = null;
+            this.QueueSize = 0;
+        }
+
+        public void push(int entry)    //Function to insert entry in Queue
+        {
+            Node nNode = new Node(entry);
+            if (this.head == null) {
+                this.head = nNode;
+                this.tail = nNode;
+                this.QueueSize = this.QueueSize+1;
+            }
+
+            else {
+                this.tail.next = nNode;
+                this.tail = nNode;
+                this.QueueSize = this.QueueSize+1;
+            }
+        }
+
+        public int pop()   //Function to delete entry in deQueue
+        {
+            if (this.head == null){ 
+                return 0;
+            }
+
+            else {
+                this.QueueSize = this.QueueSize-1;
+                int deleteData = 0;
+                deleteData = this.head.data;
+                this.head = this.head.next;
+                return deleteData;
+            }
+
+        }
+    
+        public int sizeOfQueue()  //This function will return size of Queue
+        {
+            return this.QueueSize;
+        }
+        
+        public int queueHead()   //This Function will return head of Queue
+        {
+            return this.head.data;
+        }
+        public int queueTail()   //This Function will return head of Queue
+        {
+            return this.tail.data;
+        }
+    
 
     class Program
     {
